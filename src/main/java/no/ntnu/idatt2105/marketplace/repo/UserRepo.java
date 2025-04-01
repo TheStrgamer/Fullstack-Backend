5886a62ef6c1b08903505503e544437ca2634575
jonasreiher <jonareih@stud.ntnu.no>
2025-04-01 11:43:10 +0200

    feat: added get user by id

--- src/main/java/no/ntnu/idatt2105/marketplace/repo/UserRepo.java
+++ src/main/java/no/ntnu/idatt2105/marketplace/repo/UserRepo.java
@@ -6,9 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepo extends JpaRepository<User, Integer> {
-  Optional<User> findUserByEmail(String email);
+
+  // get user by email
+  Optional<User> findByEmail(String email);
 
   // save function to save user in bd is included by default
   // usage: UserRepo.save(userObj)
 
+  // get user by id
+  Optional<User> findById(int id);
 }
