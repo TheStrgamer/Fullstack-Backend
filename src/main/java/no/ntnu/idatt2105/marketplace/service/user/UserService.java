2a6243816c33f7ee7432257c6e898463f322b1c4
jonasreiher <jonareih@stud.ntnu.no>
2025-04-02 13:08:57 +0200

    fix: added logging to UserService.java

--- src/main/java/no/ntnu/idatt2105/marketplace/service/user/UserService.java
+++ src/main/java/no/ntnu/idatt2105/marketplace/service/user/UserService.java
@@ -15,11 +15,16 @@ public class UserService {
   public User updateUser(int id, UserUpdate dto) throws Exception{
     User user = userRepo.findById(id).orElseThrow(() -> new Exception("User not found"));
 
+
+    // TODO: implement logger
+    System.out.println("Updating user: " + user.getId() + " update info: " + dto.toString());
+
     user.setFirstname(dto.getFirstname() != null              ? dto.getFirstname()        : user.getFirstname());
     user.setSurname(dto.getSurname() != null                  ? dto.getSurname()          : user.getSurname());
     user.setPhonenumber(dto.getPhonenumber() != null          ? dto.getPhonenumber()      : user.getPhonenumber());
     user.setProfile_picture(dto.getProfile_picture() != null  ? dto.getProfile_picture()  : user.getProfile_picture());
 
+    System.out.println("Updated user");
     return userRepo.save(user);
   }
 }
