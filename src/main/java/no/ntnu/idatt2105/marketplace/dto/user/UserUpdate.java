f151303cc693758fee2b7fb4979df159d81f6f60
jonasreiher <jonareih@stud.ntnu.no>
2025-04-05 14:57:02 +0200

    feat: swagger documentation for UserUpdate

--- src/main/java/no/ntnu/idatt2105/marketplace/dto/user/UserUpdate.java
+++ src/main/java/no/ntnu/idatt2105/marketplace/dto/user/UserUpdate.java
@@ -1,13 +1,24 @@
 package no.ntnu.idatt2105.marketplace.dto.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import no.ntnu.idatt2105.marketplace.model.other.Images;
 
+@Schema(description = "Data transfer object representing user update data")
 public class UserUpdate {
 
+  @Schema(description = "Updated firstname", example = "Foo")
   private String firstname;
+
+  @Schema(description = "Updated surname", example = "Boo")
   private String surname;
+
+  @Schema(description = "Updated email", example = "new.example@email.com")
   private String email;
+
+  @Schema(description = "Updated phonenumber", example = "12345678")
   private String phonenumber;
+
+  @Schema(description = "Updated profile picture metadata")
   private Images profile_picture;
 
   // getters
