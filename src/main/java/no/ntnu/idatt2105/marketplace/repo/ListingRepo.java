1b66ff28eff794aa75e098053609023277e78b16
jonasreiher <jonareih@stud.ntnu.no>
2025-04-07 11:29:50 +0200

    fix: making the project build

--- src/main/java/no/ntnu/idatt2105/marketplace/repo/ListingRepo.java
+++ src/main/java/no/ntnu/idatt2105/marketplace/repo/ListingRepo.java
@@ -20,6 +20,4 @@ public interface ListingRepo extends JpaRepository<Listing, Integer> {
     // get all listings by title
     List<Listing> findAllByTitle(String title);
 
-    // gat all listings
-    List<Listing> findAll();
 }
